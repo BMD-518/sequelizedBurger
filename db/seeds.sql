8f6bcbc62b2ef974cb790f8bd06c2beca9de5aa9
Bijan Dayani <bdayani518@gmail.com>
2019-12-05 15:04:38 -0500

    seeds file created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Fried Egg Burger'); 
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger'); 
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger'); 
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger w/Bacon'); 
