ab1bd01917803719ab08e2756295066c2c654d6c
Bijan Dayani <bdayani518@gmail.com>
2019-12-04 23:43:58 -0500

    server.js required npm packages

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
+var express = require('express');
 
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var expHb = require('express-handlebars');
+
+app.listen(PORT, function() {
+    console.log('Server listening on: http://localhost:' + PORT);
+});
 
