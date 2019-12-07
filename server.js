ee1bf92609f02f171daf917434ef1466a2c63b24
Bijan Dayani <bdayani518@gmail.com>
2019-12-07 01:13:05 -0500

    static page, handlebars setup, imported routes

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Display static page content
+app.use(express.static('public'));
+
+// Parse as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var expHb = require('express-handlebars');
+// Set handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes for server access
+var routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log('Server listening on: http://localhost:' + PORT);
