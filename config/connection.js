d57f51d2d8548734bebe1c1f12aced9285e77173
Bijan Dayani <bdayani518@gmail.com>
2019-12-17 03:49:35 -0500

    npm installs, database configuration for sequelize in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,51 @@
-var mysql = require('mysql');
+var Sequelize = require('sequelize');
 
-var connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'password',
-        database: 'burgers_db'
-    });
-};
-
-connection.connect(function(err) {
-    if (err) {
-        console.error("ERROR CONNECTING: " + err.stack);
-        return;
+const sequelize = new Sequelize('burgers_db', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
     }
-    console.log('Connected as ID: ' + connection.threadId);
 });
 
-module.exports = connection;
+// var connection;
+
+
+// JAWSDB CONNECTION
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         port: 3306,
+//         user: 'root',
+//         password: 'password',
+//         database: 'burgers_db'
+//     });
+// };
+
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Successful Connection!');
+    })
+    .catch(err => {
+        console.log('Connection failed, \nUnable to connect to the database :(')
+    });
+
+
+
+
+// OLD
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("ERROR CONNECTING: " + err.stack);
+//         return;
+//     }
+//     console.log('Connected as ID: ' + connection.threadId);
+// });
+
+module.exports = sequelize;
